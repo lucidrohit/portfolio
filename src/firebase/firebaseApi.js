5ff9b2bbd25fc0d8a7e77a87abfb7e2bfefe499f
Rohit Gupta <lucidrohit@gmail.com>
2022-09-19 12:57:42 +0530

    integrated firebase api to read blogs

---
+++ src/firebase/firebaseApi.js
@@ -0,0 +1,34 @@
+import { db } from "./firebaseConfig";
+import {getDate} from "./../utils/dateHandling"
+import {
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  query,
+  orderBy,
+  limit,
+} from "@firebase/firestore";
+
+const blogsCollectionRef = collection(db, "blogs");
+const q = query(blogsCollectionRef, orderBy("date"), limit(5));
+
+export const getBlogs = async () => {
+  const { docs } = await getDocs(q);
+  
+  return docs.map((doc) => {
+    const blog = doc.data();
+    const date = getDate(blog.date.seconds)
+    return {
+      ...blog,
+      id: doc.id,
+      date: date,
+    };
+  });
+};
+
+export const getBlog = async (blogId) => {
+  const docRef = doc(db, "blogs", blogId);
+  const blog = await getDoc(docRef);
+  return blog.data();
+};
