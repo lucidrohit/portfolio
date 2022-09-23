ecdea29e341d9ebff9354574e5949dda9322ca7f
Rohit Gupta <lucidrohit@gmail.com>
2022-09-23 18:49:18 +0530

    added setblog and date property

--- src/firebase/firebaseApi.js
+++ src/firebase/firebaseApi.js
@@ -1,5 +1,5 @@
 import { db } from "./firebaseConfig";
-import {getDate} from "./../utils/dateHandling"
+import { getDate } from "./../utils/dateHandling";
 import {
   collection,
   getDocs,
@@ -8,7 +8,9 @@ import {
   query,
   orderBy,
   limit,
+  addDoc,
 } from "@firebase/firestore";
+import { async } from "@firebase/util";
 
 const blogsCollectionRef = collection(db, "blogs");
 
@@ -16,10 +18,10 @@ const q = query(blogsCollectionRef, orderBy("date"), limit(5));
 
 export const getBlogs = async () => {
   const { docs } = await getDocs(q);
-  
+
   return docs.map((doc) => {
     const blog = doc.data();
-    const date = getDate(blog.date.seconds)
+    const date = getDate(blog.date.seconds);
     return {
       ...blog,
       id: doc.id,
@@ -31,13 +33,9 @@ export const getBlogs = async () => {
 export const getBlog = async (blogId) => {
   const docRef = doc(db, "blogs", blogId);
   const blog = await getDoc(docRef);
-  console.log(blog);
   return blog.data();
 };
 
-// const adminsCollectionRef = collection(db, "admins");
-
-// export const checkIsAdminUser = (uuid)=>{
-  // const adminDocRef = doc()
-  // const doc = getDoc()
-// }
+export const setBlog = async (blog) => {
+  await addDoc(blogsCollectionRef, blog);
+};
